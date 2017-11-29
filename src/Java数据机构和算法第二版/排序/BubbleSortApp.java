36da06d67daaaedb9c65e674d6a3c030d184e77b
founder <747060040@qq.com>
2017-11-29 16:01:30 +0800

    20171129

--- src/Java数据机构和算法第二版/排序/BubbleSortApp.java
+++ src/Java数据机构和算法第二版/排序/BubbleSortApp.java
@@ -1,5 +1,11 @@
 package Java数据机构和算法第二版.排序;
 
+/**
+ * 冒泡排序
+ * 
+ * @author founder
+ *
+ */
 public class BubbleSortApp {
 	public static void main(String[] args) {
 		ArrayBub a = new ArrayBub(10);
@@ -13,37 +19,44 @@ public class BubbleSortApp {
 		a.insert(9);
 		a.insert(5);
 		a.insert(0);
+		a.disPlay();
 		a.bubbleSort();
 		a.disPlay();
 	}
 }
-class ArrayBub{
-	private long[]	a ;
+
+class ArrayBub {
+	private long[] a;
 	private int nElems;
-	
+
 	public ArrayBub(int max) {
 		a = new long[max];
 		nElems = 0;
 	}
+
 	public void insert(long value) {
-		a[nElems++]=value;
+		a[nElems++] = value;
 	}
+
 	public void disPlay() {
 		for (long l : a) {
-			System.out.print(l+"  ");
+			System.out.print(l + "  ");
 		}
+		System.out.println();
 	}
+
 	public void bubbleSort() {
 		int length = a.length;
-		for(int i = 0;i < length;i++) {
-			for(int j = i + 1;j< length;j++) {
-				if(a[i] > a[j]) {
-					swap(i,j);
+		for (int i = 0; i < length - 1; i++) { // 最多做n-1趟排序
+			for (int j = 0; j < length - i - 1; j++) { // 对当前无序区间a[0......length-i-1]进行排序(j的范围很关键，这个范围是在逐步缩小的)
+				if(a[j]>a[j+1]) {
+					swap(j,j+1);
 				}
 			}
 		}
 	}
-	public void swap(int min,int max) {
+
+	public void swap(int min, int max) {
 		long temp = a[min];
 		a[min] = a[max];
 		a[max] = temp;
